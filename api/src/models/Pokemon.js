07fd6c86ebb77fc91cc89079c3dc4fac7dd4f3cf
agustinrc92 <agustinrodriguezconstenla@gmail.com>
2022-04-26 12:43:29 -0300

    BackEnd Advanced

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    life: {
+    hp: {
       type: DataTypes.INTEGER,
     },
     attack: {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+//Traigo Axios
+const axios = require("axios");
+//Traigo Router
 const router = Router();
+//Traigo Tablas de la db
+const { Pokemon, Type, PokemonType } = require("../db");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 
 module.exports = router;
